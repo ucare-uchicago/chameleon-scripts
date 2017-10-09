64e5f63606b5be14374b940e2fb07f60e70e9b53
Riza Suminto <riza.on@gmail.com>
2017-10-09 18:35:22 -0500

    add swift object creation script

---
+++ openstack-container-create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# example
+# openstack container create container-name
+
+openstack container create $1
---
+++ openstack-object-create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# example
+# openstack object create container-name filepath
+
+openstack object create $1 $2
+

385d94b769e6c0401cdaa7008671fa958321c4dd
Riza Suminto <riza.on@gmail.com>
2017-10-11 00:35:54 +0000

    change server creation back to newton style

--- openstack-server-create.sh
+++ openstack-server-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 openstack server create --flavor m1.small --image trusty-java \
-	  --network public --security-group ssh \
-	    --key-name ucare-chameleon server-name
+	  --nic net-id=35ee448b-459c-4b28-817e-1b3f4c7deda6 --security-group demo-ssh \
+	    --key-name ucare-chameleon test
 

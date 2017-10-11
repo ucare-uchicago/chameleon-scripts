4dc74c71a8f11509eb01f0f6a61aaf39a92e8ff2
Riza Suminto <riza.on@gmail.com>
2017-10-11 00:34:36 +0000

    add command to create new security group

--- openstack-security-group-create-ssh.sh
+++ openstack-security-group-create-ssh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 openstack security group create demo-ssh
+openstack security group rule create --ingress --protocol icmp demo-ssh
 openstack security group rule create --ingress --dst-port 22 demo-ssh
+openstack security group rule create --ingress --dst-port 80 demo-ssh
 

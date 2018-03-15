b2d0b70330fb5a261d82bde4bd538aa5fba95817
Riza Suminto <riza.on@gmail.com>
2018-03-15 18:59:30 -0500

    add enos config

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -2,6 +2,5 @@
 
 virtualenv --python=/usr/bin/python2.7 .
 source bin/activate
-pip install enos
-pip install git+https://github.com/openstack/python-blazarclient
+pip install -r enos-requirement.txt
 

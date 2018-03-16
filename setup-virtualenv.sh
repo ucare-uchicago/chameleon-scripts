4b0ef4c1b52350795a205f5ad7fa7727df72a8c6
Riza Suminto <riza.on@gmail.com>
2018-03-16 00:42:51 +0000

    update setup-virtualenv

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-virtualenv --python=/usr/bin/python2.7 .
-source bin/activate
+sudo apt-get update
+sudo apt-get install -y virtualenv python-dev
+virtualenv --python=/usr/bin/python2.7 venv
+source venv/bin/activate
 pip install -r enos-requirement.txt
 
